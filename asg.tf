e91201e96dfa75b71f7520da37b51b87138f7984
Phillip <phillip.shipley@gmail.com>
2020-08-10 18:03:59 -0400

    update to TF 0.12 compatibility

--- asg.tf
+++ asg.tf
@@ -2,10 +2,10 @@
  * Generate user_data from template file
  */
 data "template_file" "user_data" {
-  template = "${file("${path.module}/default-user-data.sh")}"
+  template = file("${path.module}/default-user-data.sh")
 
-  vars {
-    ecs_cluster_name = "${var.cluster_name}"
+  vars = {
+    ecs_cluster_name = var.cluster_name
   }
 }
 
@@ -13,16 +13,16 @@ data "template_file" "user_data" {
  * Create Launch Configuration
  */
 resource "aws_launch_configuration" "lc" {
-  image_id             = "${data.aws_ami.ecs_ami.id}"
-  name_prefix          = "${var.cluster_name}"
-  instance_type        = "${var.instance_type}"
-  iam_instance_profile = "${aws_iam_instance_profile.ecsInstanceProfile.id}"
-  security_groups      = ["${var.security_groups}"]
-  user_data            = "${var.user_data != "false" ? var.user_data : data.template_file.user_data.rendered}"
-  key_name             = "${var.ssh_key_name}"
+  image_id             = data.aws_ami.ecs_ami.id
+  name_prefix          = var.cluster_name
+  instance_type        = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.ecsInstanceProfile.id
+  security_groups      = [var.security_groups]
+  user_data            = var.user_data != "false" ? var.user_data : data.template_file.user_data.rendered
+  key_name             = var.ssh_key_name
 
   root_block_device {
-    volume_size = "${var.root_volume_size}"
+    volume_size = var.root_volume_size
   }
 
   lifecycle {
@@ -34,24 +34,24 @@ resource "aws_launch_configuration" "lc" {
  * Create Auto-Scaling Group
  */
 resource "aws_autoscaling_group" "asg" {
-  name                      = "${var.cluster_name}"
-  vpc_zone_identifier       = ["${var.subnet_ids}"]
-  min_size                  = "${var.min_size}"
-  max_size                  = "${var.max_size}"
-  health_check_type         = "${var.health_check_type}"
-  health_check_grace_period = "${var.health_check_grace_period}"
-  default_cooldown          = "${var.default_cooldown}"
-  termination_policies      = ["${var.termination_policies}"]
-  launch_configuration      = "${aws_launch_configuration.lc.id}"
+  name                      = var.cluster_name
+  vpc_zone_identifier       = [var.subnet_ids]
+  min_size                  = var.min_size
+  max_size                  = var.max_size
+  health_check_type         = var.health_check_type
+  health_check_grace_period = var.health_check_grace_period
+  default_cooldown          = var.default_cooldown
+  termination_policies      = [var.termination_policies]
+  launch_configuration      = aws_launch_configuration.lc.id
 
-  tags = ["${concat(
+  tags = [concat(
     list(
       map("key", "ecs_cluster", "value", var.cluster_name, "propagate_at_launch", true)
     ),
     var.tags
-  )}"]
+  )]
 
-  protect_from_scale_in = "${var.protect_from_scale_in}"
+  protect_from_scale_in = var.protect_from_scale_in
 
   lifecycle {
     create_before_destroy = true
@@ -63,22 +63,22 @@ resource "aws_autoscaling_group" "asg" {
  */
 resource "aws_autoscaling_policy" "up" {
   name                   = "${var.cluster_name}-scaleUp"
-  scaling_adjustment     = "${var.scaling_adjustment_up}"
-  adjustment_type        = "${var.adjustment_type}"
-  cooldown               = "${var.policy_cooldown}"
+  scaling_adjustment     = var.scaling_adjustment_up
+  adjustment_type        = var.adjustment_type
+  cooldown               = var.policy_cooldown
   policy_type            = "SimpleScaling"
-  autoscaling_group_name = "${aws_autoscaling_group.asg.name}"
-  count                  = "${var.alarm_actions_enabled ? 1 : 0}"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  count                  = var.alarm_actions_enabled ? 1 : 0
 }
 
 resource "aws_autoscaling_policy" "down" {
   name                   = "${var.cluster_name}-scaleDown"
-  scaling_adjustment     = "${var.scaling_adjustment_down}"
-  adjustment_type        = "${var.adjustment_type}"
-  cooldown               = "${var.policy_cooldown}"
+  scaling_adjustment     = var.scaling_adjustment_down
+  adjustment_type        = var.adjustment_type
+  cooldown               = var.policy_cooldown
   policy_type            = "SimpleScaling"
-  autoscaling_group_name = "${aws_autoscaling_group.asg.name}"
-  count                  = "${var.alarm_actions_enabled ? 1 : 0}"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  count                  = var.alarm_actions_enabled ? 1 : 0
 }
 
 /*
@@ -88,18 +88,18 @@ resource "aws_cloudwatch_metric_alarm" "scaleUp" {
   alarm_name          = "${var.cluster_name}-scaleUp"
   alarm_description   = "ECS cluster scaling metric above threshold"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
-  metric_name         = "${var.scaling_metric_name}"
+  evaluation_periods  = var.evaluation_periods
+  metric_name         = var.scaling_metric_name
   namespace           = "AWS/ECS"
   statistic           = "Average"
-  period              = "${var.alarm_period}"
-  threshold           = "${var.alarm_threshold_up}"
-  actions_enabled     = "${var.alarm_actions_enabled}"
-  count               = "${var.alarm_actions_enabled ? 1 : 0}"
-  alarm_actions       = ["${aws_autoscaling_policy.up.arn}"]
+  period              = var.alarm_period
+  threshold           = var.alarm_threshold_up
+  actions_enabled     = var.alarm_actions_enabled
+  count               = var.alarm_actions_enabled ? 1 : 0
+  alarm_actions       = [aws_autoscaling_policy.up.arn]
 
-  dimensions {
-    ClusterName = "${var.cluster_name}"
+  dimensions = {
+    ClusterName = var.cluster_name
   }
 }
 
@@ -107,18 +107,18 @@ resource "aws_cloudwatch_metric_alarm" "scaleDown" {
   alarm_name          = "${var.cluster_name}-scaleDown"
   alarm_description   = "ECS cluster scaling metric under threshold"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
-  metric_name         = "${var.scaling_metric_name}"
+  evaluation_periods  = var.evaluation_periods
+  metric_name         = var.scaling_metric_name
   namespace           = "AWS/ECS"
   statistic           = "Average"
-  period              = "${var.alarm_period}"
-  threshold           = "${var.alarm_threshold_down}"
-  actions_enabled     = "${var.alarm_actions_enabled}"
-  count               = "${var.alarm_actions_enabled ? 1 : 0}"
-  alarm_actions       = ["${aws_autoscaling_policy.down.arn}"]
+  period              = var.alarm_period
+  threshold           = var.alarm_threshold_down
+  actions_enabled     = var.alarm_actions_enabled
+  count               = var.alarm_actions_enabled ? 1 : 0
+  alarm_actions       = [aws_autoscaling_policy.down.arn]
 
-  dimensions {
-    ClusterName = "${var.cluster_name}"
+  dimensions = {
+    ClusterName = var.cluster_name
   }
 }
 
@@ -126,7 +126,7 @@ resource "aws_cloudwatch_metric_alarm" "scaleDown" {
  * ASG related variables
  */
 
-// Reqiured:
+// Required:
 variable "security_groups" {
   description = "List of security groups to place instances into"
   type        = "list"
--- ecs.tf
+++ ecs.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ecs_ami" {
  * Create ECS cluster
  */
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "${var.cluster_name}"
+  name = var.cluster_name
 }
 
 /*
@@ -27,13 +27,13 @@ resource "random_id" "code" {
 
 resource "aws_iam_role" "ecsInstanceRole" {
   name               = "ecsInstanceRole-${random_id.code.hex}"
-  assume_role_policy = "${var.ecsInstanceRoleAssumeRolePolicy}"
+  assume_role_policy = var.ecsInstanceRoleAssumeRolePolicy
 }
 
 resource "aws_iam_role_policy" "ecsInstanceRolePolicy" {
   name   = "ecsInstanceRolePolicy-${random_id.code.hex}"
-  role   = "${aws_iam_role.ecsInstanceRole.id}"
-  policy = "${var.ecsInstancerolePolicy}"
+  role   = aws_iam_role.ecsInstanceRole.id
+  policy = var.ecsInstancerolePolicy
 }
 
 /*
@@ -41,18 +41,18 @@ resource "aws_iam_role_policy" "ecsInstanceRolePolicy" {
  */
 resource "aws_iam_role" "ecsServiceRole" {
   name               = "ecsServiceRole-${random_id.code.hex}"
-  assume_role_policy = "${var.ecsServiceRoleAssumeRolePolicy}"
+  assume_role_policy = var.ecsServiceRoleAssumeRolePolicy
 }
 
 resource "aws_iam_role_policy" "ecsServiceRolePolicy" {
   name   = "ecsServiceRolePolicy-${random_id.code.hex}"
-  role   = "${aws_iam_role.ecsServiceRole.id}"
-  policy = "${var.ecsServiceRolePolicy}"
+  role   = aws_iam_role.ecsServiceRole.id
+  policy = var.ecsServiceRolePolicy
 }
 
 resource "aws_iam_instance_profile" "ecsInstanceProfile" {
   name = "ecsInstanceProfile-${random_id.code.hex}"
-  role = "${aws_iam_role.ecsInstanceRole.name}"
+  role = aws_iam_role.ecsInstanceRole.name
 }
 
 /*
